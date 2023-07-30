1966ca4107c09fa09e8f27bb089fc74d32ca2b46
joao pedro <joao.ferreira01br@gmail.com>
2023-07-30 17:50:41 -0300

    Deixando mais clean o programa

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,10 +1,7 @@
 package application;
 
-import java.util.Date;
-
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
-import model.entities.Department;
 import model.entities.Seller;
 
 public class Program {
@@ -14,8 +11,8 @@ public class Program {
 		
 		SellerDao sellerDao = DaoFactory.createSellerDao();
 		
+		System.out.println("==== TEST 1: seller findByld ====");
 		Seller seller = sellerDao.findByld(3);
-		
 		System.out.println(seller);
 	
 	}
