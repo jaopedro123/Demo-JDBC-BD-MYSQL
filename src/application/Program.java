2c5eb9c5ea0f25a582fb1aaeeb8c939e0aa8cc71
joao pedro <joao.ferreira01br@gmail.com>
2023-07-30 19:03:18 -0300

    implementaçao do DELETE

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,6 +2,7 @@ package application;
 
 import java.util.Date;
 import java.util.List;
+import java.util.Scanner;
 
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
@@ -12,6 +13,7 @@ public class Program {
 
 	public static void main(String[] args) {
 		
+		Scanner sc = new Scanner(System.in);
 		
 		SellerDao sellerDao = DaoFactory.createSellerDao();
 		
@@ -31,7 +33,7 @@ public class Program {
 		for (Seller obj : list) {
 			System.out.println(obj);
 		}
-		
+	
 		System.out.println("\n==== TEST 4: seller Insert ====");
 		Seller newSeller = new Seller(null, "Greg", "greg@gmail.com", new Date(), 4000.0, department);
 		sellerDao.insert(newSeller);
@@ -42,6 +44,13 @@ public class Program {
 		seller.setName("Martha Waine");
 		sellerDao.update(seller);
 		System.out.println("Update completed");
+	
+		System.out.println("\n==== TEST 6: seller Delete ====");
+		System.out.print("Entre com ID para o teste de deletar: ");
+		int id = sc.nextInt();
+		sellerDao.deleteByld(id);
+		System.out.println("Delete completed");
 		
+		sc.close();
 	}
 }
