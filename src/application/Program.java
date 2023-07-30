7f9a9da3cc81ac800139b4099955edf126335889
joao pedro <joao.ferreira01br@gmail.com>
2023-07-30 17:43:27 -0300

    findByld implementaçao

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,6 +2,8 @@ package application;
 
 import java.util.Date;
 
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
@@ -9,8 +11,10 @@ public class Program {
 
 	public static void main(String[] args) {
 		
-		Department obj = new Department(1, "Books");
-		Seller seller = new Seller(21, "Bob", "Bob@gmail.com", new Date(), 300.00, obj);
+		
+		SellerDao sellerDao = DaoFactory.createSellerDao();
+		
+		Seller seller = sellerDao.findByld(3);
 		
 		System.out.println(seller);
 	
